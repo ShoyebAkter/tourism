f16e436d1ac1b86b7da03ae90ac6981515e45b36
Shoyeb Akter <heroreal5385@gmail.com>
2023-12-12 12:20:00 +0600

    popup done

--- script/guides.js
+++ script/guides.js
@@ -3,42 +3,42 @@ const tourGuides = [
         id: 1,
         name: "Fahan",
         image: './images/guides/fahan.png',
-        description: "Fluent in English, Tamil, Bahasa Melayu.",
+        description: "Fluent in English, Tamil, Bahasa Melayu. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolor, nihil fugiat quisquam quia nam inventore ipsum voluptates explicabo sunt cupiditate tenetur eveniet ea enim nisi odio ab perspiciatis sint?",
         email: 'info@mysite.com'
     },
     {
         id: 2,
-        name: "Fahan",
-        image: './images/guides/fahan.png',
-        description: "Fluent in English, Tamil, Bahasa Melayu.",
+        name: "Fahad",
+        image: './images/guides/fahad.png',
+        description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolor, nihil fugiat quisquam quia nam inventore ipsum voluptates explicabo sunt cupiditate tenetur eveniet ea enim nisi odio ab perspiciatis sint?Fluent in English, Tamil, Bahasa Melayu.",
         email: 'info@mysite.com'
     },
     {
         id: 3,
-        name: "Fahan",
-        image: './images/guides/fahan.png',
-        description: "Fluent in English, Tamil, Bahasa Melayu.",
+        name: "Forhad",
+        image: './images/guides/forhad.png',
+        description: "Fluent in English, Tamil, Bahasa Melayu. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolor, nihil fugiat quisquam quia nam inventore ipsum voluptates explicabo sunt cupiditate tenetur eveniet ea enim nisi odio ab perspiciatis sint?",
         email: 'info@mysite.com'
     },
     {
         id: 4,
-        name: "Fahan",
-        image: './images/guides/fahan.png',
-        description: "Fluent in English, Tamil, Bahasa Melayu.",
+        name: "Tahsin",
+        image: './images/guides/tahsin.png',
+        description: "Fluent in English, Tamil, Bahasa Melayu. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolor, nihil fugiat quisquam quia nam inventore ipsum voluptates explicabo sunt cupiditate tenetur eveniet ea enim nisi odio ab perspiciatis sint?",
         email: 'info@mysite.com'
     },
     {
         id: 5,
         name: "Fahan",
         image: './images/guides/fahan.png',
-        description: "Fluent in English, Tamil, Bahasa Melayu.",
+        description: "Fluent in English, Tamil, Bahasa Melayu. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolor, nihil fugiat quisquam quia nam inventore ipsum voluptates explicabo sunt cupiditate tenetur eveniet ea enim nisi odio ab perspiciatis sint?",
         email: 'info@mysite.com'
     },
     {
         id: 6,
         name: "Fahan",
         image: './images/guides/fahan.png',
-        description: "Fluent in English, Tamil, Bahasa Melayu.",
+        description: "Fluent in English, Tamil, Bahasa Melayu. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolor, nihil fugiat quisquam quia nam inventore ipsum voluptates explicabo sunt cupiditate tenetur eveniet ea enim nisi odio ab perspiciatis sint?",
         email: 'info@mysite.com'
     }
 ]
@@ -49,9 +49,10 @@ const initApp = () => {
     itemsHTML = tourGuides.map((item, key) => `
     <div class="card">
     <div class="cover-photo">
-        <img src="${item.image}" class="profile">
+        <img src="${item.image}" class="profile"/>
+        <h3 class="profile-name">"${item.name}"</h3>
     </div>
-    <h3 class="profile-name">"${item.name}"</h3>
+    
     <p class="about">"${item.description}"</p>
     <button onclick="select()" class="btn">Select</button>
     <div class="icons">
