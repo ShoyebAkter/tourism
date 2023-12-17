d7761e711c17c4c24cf613e50ab9428323d00f9b
Shoyeb Akter <heroreal5385@gmail.com>
2023-12-17 11:13:22 +0600

    authentication logic added

--- script/homepage.js
+++
@@ -1,6 +0,0 @@
-const moveToGuide=(place,price,days)=>{
-    localStorage.setItem("place",place)
-    localStorage.setItem("price",price)
-    localStorage.setItem("days",days)
-    window.location.href="guide.html"
-}
